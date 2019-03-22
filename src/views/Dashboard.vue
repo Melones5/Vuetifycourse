2f9442dcb507a13d123a3bf05485b23d4469f3c4
Melones5 <32208091+Melones5@users.noreply.github.com>
2019-03-22 00:19:56 -0300

    Testing vuetify

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="dashboard">
+    <h1 class="subheading grey--text">Dashboard</h1>
+
+    <v-container class="my-5">
+
+      <v-layout row class="mb-3">
+        <v-tooltip top>
+          <v-btn small flat color="grey" @click="sortBy('title')" slot="activator">
+            <v-icon left small>folder</v-icon>
+            <span class="caption text-lowercase">by project name</span>
+          </v-btn>
+          <span>Sort projects by project name</span>
+        </v-tooltip>
+
+        <v-tooltip top>
+          <v-btn small flat color="grey" @click="sortBy('person')" slot="activator">
+            <v-icon left small>person</v-icon>
+            <span class="caption text-lowercase">by person</span>
+          </v-btn>
+          <span>Sort project by person</span>
+        </v-tooltip>
+
+      </v-layout>
+
+      <v-card flat v-for="project in projects" :key="project.title">
+        <v-layout row wrap :class="`pa-3 project ${project.status}`">
+          <v-flex xs12 md6>
+            <div class="caption grey--text">Project Title</div>
+            <div>{{ project.title }}</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Person</div>
+            <div>{{ project.person }}</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Due by</div>
+            <div>{{ project.due }}</div>
+          </v-flex>
+          <v-flex xs2 sm4 md2>
+            <div class="right">
+              <v-chip
+                small
+                :class="`${project.status} white--text caption my-2`"
+              >{{ project.status }}</v-chip>
+            </div>
+          </v-flex>
+        </v-layout>
+        <v-divider></v-divider>
+      </v-card>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      projects: [
+        {
+          title: "Desing a new website",
+          person: "The Net Ninja",
+          due: "1st Jan 2019",
+          status: "ongoing",
+          content:
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. At quis laboriosam, asperiores quod perspiciatis, repudiandae earum quidem dignissimos amet sunt repellat error cupiditate culpa deleniti quos, modi quaerat obcaecati nesciunt!"
+        },
+        {
+          title: "Code up the homepage",
+          person: "Chun Li",
+          due: "10th Jan 2019",
+          status: "complete",
+          content:
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. At quis laboriosam, asperiores quod perspiciatis, repudiandae earum quidem dignissimos amet sunt repellat error cupiditate culpa deleniti quos, modi quaerat obcaecati nesciunt!"
+        },
+        {
+          title: "Desing video thumbnails",
+          person: "Ryu",
+          due: "20th Dec 2018",
+          status: "complete",
+          content:
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. At quis laboriosam, asperiores quod perspiciatis, repudiandae earum quidem dignissimos amet sunt repellat error cupiditate culpa deleniti quos, modi quaerat obcaecati nesciunt!"
+        },
+        {
+          title: "Create a community forum",
+          person: "Gouken",
+          due: "20th Oct 2018",
+          status: "overdue",
+          content:
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. At quis laboriosam, asperiores quod perspiciatis, repudiandae earum quidem dignissimos amet sunt repellat error cupiditate culpa deleniti quos, modi quaerat obcaecati nesciunt!"
+        }
+      ]
+    };
+  },
+  methods: {
+    sortBy(prop) {
+      this.projects.sort((a, b) => (a[prop] < b[prop] ? -1 : 1));
+    }
+  }
+};
+</script>
+
+
+<style>
+.project.complete {
+  border-left: 4px solid #3cd1c2;
+}
+.project.ongoing {
+  border-left: 4px solid orange;
+}
+.project.overdue {
+  border-left: 4px solid tomato;
+}
+.v-chip.complete {
+  background: #3cd1c2;
+}
+.v-chip.ongoing {
+  background: orange;
+}
+.v-chip.overdue {
+  background: tomato;
+}
+</style>
---
+++ src/views/Projects.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="projects">
+    <h1 class="subheading grey--text">Projects</h1>
+
+
+    <v-container class="my-5">
+      
+      <v-expansion-panel>
+        <v-expansion-panel-content v-for="project in myProjects" :key="project.title">
+          <div slot="header">{{ project.title }}</div>
+          <v-card>
+            <v-card-text class="px-4 grey--text">
+              <div class="font-weight-bold">due by {{ project.due }}</div>
+              <div>{{ project.content }}</div>
+            </v-card-text>
+          </v-card>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return{
+       projects: [
+        {
+          title: "Desing a new website",
+          person: "The Net Ninja",
+          due: "1st Jan 2019",
+          status: "ongoing",
+          content:
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. At quis laboriosam, asperiores quod perspiciatis, repudiandae earum quidem dignissimos amet sunt repellat error cupiditate culpa deleniti quos, modi quaerat obcaecati nesciunt!"
+        },
+        {
+          title: "Code up the homepage",
+          person: "Chun Li",
+          due: "10th Jan 2019",
+          status: "complete",
+          content:
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. At quis laboriosam, asperiores quod perspiciatis, repudiandae earum quidem dignissimos amet sunt repellat error cupiditate culpa deleniti quos, modi quaerat obcaecati nesciunt!"
+        },
+        {
+          title: "Desing video thumbnails",
+          person: "Ryu",
+          due: "20th Dec 2018",
+          status: "complete",
+          content:
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. At quis laboriosam, asperiores quod perspiciatis, repudiandae earum quidem dignissimos amet sunt repellat error cupiditate culpa deleniti quos, modi quaerat obcaecati nesciunt!"
+        },
+        {
+          title: "Create a community forum",
+          person: "Gouken",
+          due: "20th Oct 2018",
+          status: "overdue",
+          content:
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. At quis laboriosam, asperiores quod perspiciatis, repudiandae earum quidem dignissimos amet sunt repellat error cupiditate culpa deleniti quos, modi quaerat obcaecati nesciunt!"
+        },
+      ]
+    }
+  },
+  computed: {
+    myProjects (){
+      return this.projects.filter(project => {
+        return project.person === 'The Net Ninja'
+      })
+    }
+  }
+};
+</script>
+
